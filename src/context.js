e0d4060839365d26f33e91630f984bfd3a348efe
Jose <jlcab8@gmail.com>
2022-05-22 19:50:47 +0200

    added clear items functionality withuseReducer

--- src/context.js
+++ src/context.js
@@ -13,13 +13,27 @@ const initialState = {
   amount: 0,
 };
 
+/* Una vez terminado el setup básico, para montar las funcionalidades
+seguiremos 3 pasos:
+1º En el context dentro de AppProvider montamos la función que contiene un dispatch({type: "XXXXX_XXXXX"})
+2º En el context pasamos la función a través del value del Provider
+3º Recibimos la función en el componente que la va a usar
+4º Utilizamos la función en el sitio que corresponda del JSX
+4º En el reducer cambiamos el estado en base al tipo de acción
+*/
+
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
 
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
       }}
     >
       {children}
