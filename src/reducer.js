249a022818ad60c86242aa77275048b3e452a22a
Jose <jlcab8@gmail.com>
2022-05-22 20:13:38 +0200

    added remove single item functionality

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,11 @@ const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
   }
+
+  if (action.type === "REMOVE") {
+    const newItems = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: newItems };
+  }
   return state;
 };
 
