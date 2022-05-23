c263ef4c1cc3d4cdb604a1f76875de00dc233e26
Jose <jlcab8@gmail.com>
2022-05-23 09:15:49 +0200

    added increase functionality

--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,24 @@ const reducer = (state, action) => {
   }
 
   if (action.type === "REMOVE") {
-    const newItems = state.cart.filter((item) => item.id !== action.payload);
-    return { ...state, cart: newItems };
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload),
+    };
+  }
+
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+
+    return {
+      ...state,
+      cart: tempCart,
+    };
   }
   return state;
 };
